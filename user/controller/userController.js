aa078cb84df84f668409a7bc155b4d775240c0c0
theressangelina <153657129+theressangelina@users.noreply.github.com>
2024-06-26 14:33:14 +0700

    Create userController.js

---
+++ user/controller/userController.js
@@ -0,0 +1,20 @@
+import User from '../model/userModel.js';
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const newUser = await User.create({ name, email, password });
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
