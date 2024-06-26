0d91fdaaed8cdd4bc46a493fc6ee558b3e5d5822
theressangelina <153657129+theressangelina@users.noreply.github.com>
2024-06-26 14:37:48 +0700

    Create productModel.js

---
+++ product/model/productModel.js
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../database.js';
+
+const Product = sequelize.define('Product', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+    }
+});
+
+export default Product;
